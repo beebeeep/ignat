c43e291eb555e6dedb942ea70f20abab306516db
Migalin Danila <me@miga.me.uk>
2017-05-31 07:18:19 +0000

    chain collection

--- ignat.go
+++ ignat.go
@@ -5,13 +5,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
-	"os"
 	"regexp"
+	"strings"
 
 	"github.com/boltdb/bolt"
 	"gopkg.in/yaml.v2"
 )
+
+type Chain map[string][]string
 
 type BotConfig struct {
 	Token    string `yaml:"token"`
@@ -22,6 +25,7 @@ type BotConfig struct {
 type IgnatBot struct {
 	config BotConfig
 	db     *bolt.DB
+	chain  Chain
 }
 
 func (bot *IgnatBot) Init(filename string) error {
@@ -39,6 +43,7 @@ func (bot *IgnatBot) Init(filename string) error {
 		panic(err)
 	}
 	bot.db = db
+	bot.chain = bot.MakeChain()
 
 	return nil
 }
@@ -69,9 +74,9 @@ func (bot *IgnatBot) ProcessUpdate(body []byte) {
 
 		text := update.Message.Text
 		if matched, _ := regexp.MatchString(".*[Дд]а$", text); matched {
-			bot.ApiPost("sendMessage", map[string]interface{}{
-				"chat_id": update.Message.Chat.Id,
-				"text":    "Пизда!"})
+			/*bot.ApiPost("sendMessage", map[string]interface{}{
+			"chat_id": update.Message.Chat.Id,
+			"text":    "Пизда!"})*/
 		}
 	}
 }
@@ -80,6 +85,29 @@ func (bot *IgnatBot) UpdateHandler(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
 	fmt.Printf("Got payload %v\n", string(body))
 	go bot.ProcessUpdate(body)
+}
+
+func (bot *IgnatBot) DumpHandler(w http.ResponseWriter, r *http.Request) {
+	stat := bot.DumpHistory()
+	s := map[string]map[string]int{
+		"result": stat,
+	}
+	body, _ := json.Marshal(s)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Write(body)
+}
+
+func (bot *IgnatBot) DumpChainHandler(w http.ResponseWriter, r *http.Request) {
+	s := map[string]interface{}{
+		"result": bot.chain,
+	}
+	body, _ := json.Marshal(s)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Write(body)
+}
+
+func (bot *IgnatBot) GenerateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(bot.MakeSentence()))
 }
 
 func (bot *IgnatBot) ApiPost(method string, body interface{}) {
@@ -96,30 +124,108 @@ func (bot *IgnatBot) ApiPost(method string, body interface{}) {
 	}
 }
 
-func main() {
-	if len(os.Args) > 1 && os.Args[1] == "dump" {
-		fmt.Println("Dumping history")
-		db, _ := bolt.Open("ignat.db", 0400, nil)
-		db.View(func(tx *bolt.Tx) error {
-			b := tx.Bucket([]byte("history"))
-			c := b.Cursor()
+func (bot *IgnatBot) MakeChain() Chain {
+	chain := make(Chain)
+	splitter := regexp.MustCompile("[ .,!?;]+")
+	printable := regexp.MustCompile("\\p{Cyrillic}+")
 
-			for k, v := c.First(); k != nil; k, v = c.Next() {
-				var msg Message
-				err := json.Unmarshal(v, &msg)
-				if err != nil {
-					fmt.Printf("Got error: %v\n", err)
-				}
-				fmt.Println(msg)
+	bot.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("history"))
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			var msg Message
+			err := json.Unmarshal(v, &msg)
+			if err != nil {
+				fmt.Printf("Got error: %v\n", err)
+				return err
 			}
-			return nil
-		})
+
+			//for _, sentence := range strings.Split(msg.Text, "\n") {
+			var words []string
+			for _, w := range splitter.Split(msg.Text, -1) {
+				if printable.Match([]byte(w)) {
+					words = append(words, w)
+				}
+			}
+			for i := 0; i < len(words)-3; i++ {
+				key := fmt.Sprintf("%v %v", words[i], words[i+1])
+				chain[key] = append(chain[key], words[3])
+			}
+			//}
+		}
+		return nil
+	})
+
+	return chain
+}
+
+func (bot *IgnatBot) MakeSentence() string {
+
+	i := 0
+	couples := make([]string, len(bot.chain))
+	fmt.Printf("%v %v\n", len(couples), len(bot.chain))
+	for c := range bot.chain {
+		couples[i] = c
+		i++
 	}
+	fmt.Printf("%v %v\n", len(couples), len(bot.chain))
+
+	couple := couples[rand.Intn(len(couples))]
+	sentence := couple
+	fmt.Printf("Start is '%v'\n", sentence)
+	for {
+		words := strings.Split(couple, " ")
+		next, ok := bot.chain[couple]
+		if !ok {
+			break
+		}
+		fmt.Printf("Couple is %v -> %v\n", couple, next)
+
+		next_word := next[rand.Intn(len(next))]
+		sentence = fmt.Sprintf("%v %v", sentence, next_word)
+		couple = fmt.Sprintf("%v %v", words[1], next_word)
+		fmt.Printf("Next couple is '%v'\n", couple)
+	}
+	return fmt.Sprint(sentence, "\n")
+}
+
+func (bot *IgnatBot) DumpHistory() map[string]int {
+	word_stat := make(map[string]int)
+	splitter := regexp.MustCompile("[ .,!?;\n]+")
+	printable := regexp.MustCompile("\\p{Cyrillic}+")
+
+	bot.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("history"))
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			var msg Message
+			err := json.Unmarshal(v, &msg)
+			if err != nil {
+				fmt.Printf("Got error: %v\n", err)
+				return err
+			}
+			for _, w := range splitter.Split(msg.Text, -1) {
+				if printable.Match([]byte(w)) {
+					word_stat[strings.ToLower(w)] += 1
+				}
+			}
+		}
+		return nil
+	})
+
+	return word_stat
+}
+
+func main() {
+
 	var ignat IgnatBot
 	ignat.Init("ignat.yaml")
-	fmt.Println(ignat)
 
 	http.HandleFunc(fmt.Sprintf("/ignat/%v", ignat.config.Token), ignat.UpdateHandler)
+	http.HandleFunc("/ignat/dump", ignat.DumpChainHandler)
+	http.HandleFunc("/ignat/generate", ignat.GenerateHandler)
 	ignat.ApiPost("setWebhook", map[string]interface{}{
 		"url":             fmt.Sprintf("%v/ignat/%v", ignat.config.HookBase, ignat.config.Token),
 		"allowed_updates": []string{"message"},
